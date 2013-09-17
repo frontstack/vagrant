932c7a4a5a7cff3cfd1422e48a1aac1cbcc0a65f
Tomas <tomas@aparicio.me>
2013-09-18 01:06:04 +0200

    Added nano package

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -21,8 +21,8 @@ INSTALLDIR='/home/vagrant/frontstack'
 $fs_bashprofile=1
 $fs_format='tar.gz'
 $fs_user='vagrant'
-$fs_download='http://dropbox.es'
-$install_packages='git gcc'
+$fs_download='http://dl.dropboxusercontent.com/u/22374892/frontstack-0.1.0-x64.tar.gz'
+$install_packages='nano git gcc'
 
 checkExitCode() {
   if [ $? -ne 0 ]; then
