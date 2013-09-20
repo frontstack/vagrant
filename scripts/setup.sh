05ee856d5a96af0504435185491abf3849f94ed3
Tomas <tomas@aparicio.me>
2013-09-21 00:40:43 +0200

    Setup script refactor

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,12 +6,12 @@
 # @license WTFPL
 #
 
-SZIPURL=http://dl.bintray.com/frontstack/stable/7z/7z-9.20-x64.tar.gz
-TEMPDIR=/tmp/frontstack
-DOWNLOADSDIR=$TEMPDIR/downloads
-OUTPUTLOG=$TEMPDIR/output.log
-CONFIGFILE="$(dirname $(readlink -f "$0")})/$(echo ${0##*/} | sed 's/\.[^\.]*$//').ini"
-INSTALLDIR='/home/vagrant/frontstack'
+szip_url=http://dl.bintray.com/frontstack/stable/7z/7z-9.20-x64.tar.gz
+temporal=/tmp/frontstack
+download_dir=$temporal/downloads
+out_log=$temporal/output.log
+config_file="$(dirname $(readlink -f "$0")})/$(echo ${0##*/} | sed 's/\.[^\.]*$//').ini"
+install_dir='/home/vagrant/frontstack'
 
 # default install options (you can customize them from setup.ini)
 fs_bash_profile='1'
@@ -20,14 +20,14 @@ fs_user='vagrant'
 fs_download='http://sourceforge.net/projects/frontstack/files/releases/0.1/frontstack-0.1.0-x64.tar.gz/download'
 install_packages='nano git gcc'
 
-checkExitCode() {
+check_exit() {
   if [ $? -ne 0 ]; then
     echo $1
     exit 1
   fi
 }
 
-makeDir() {
+make_dir() {
   if [ ! -d $1 ]; then
     mkdir $1
   fi
@@ -42,8 +42,8 @@ exists() {
   fi
 }
 
-installPackage() {
-  if [ -z $NOPKGMANAGER ]; then
+install_package() {
+  if [ -z $nopkgmanager ]; then
     # Debian, Ubuntu and derivatives (with apt-get)
     if which apt-get &> /dev/null; then
       apt-get install -y "$@"
@@ -61,44 +61,41 @@ installPackage() {
       pacman -Sy "$@"
     # Else, if no package manager has been founded
     else
-       # Set $NOPKGMANAGER
-       NOPKGMANAGER=1
+       # Set $nopkgmanager
+       nopkgmanager=1
        echo "ERROR: impossible to found a package manager in your system. Install '$@' manually"
     fi
   fi
 }
 
-configParser() {
+config_parser() {
   ini="$(<$1)"               # read the file
   ini="${ini//[/\[}"          # escape [
   ini="${ini//]/\]}"          # escape ]
-  IFS=$'\n' && ini=( ${ini} ) # convert to line-array
-  ini=( ${ini[*]//;*/} )      # remove comments with ;
-  ini=( ${ini[*]/\    =/=} )  # remove tabs before =
-  ini=( ${ini[*]/=\   /=} )   # remove tabs be =
-  ini=( ${ini[*]/\ =\ /=} )   # remove anything with a space around =
-  ini=( ${ini[*]/#\\[/\}$'\n'config.section.} ) # set section prefix
-  ini=( ${ini[*]/%\\]/ \(} )    # convert text2function (1)
-  ini=( ${ini[*]/=/=\( } )    # convert item to array
-  ini=( ${ini[*]/%/ \)} )     # close array parenthesis
-  ini=( ${ini[*]/%\\ \)/ \\} ) # the multiline trick
-  ini=( ${ini[*]/%\( \)/\(\) \{} ) # convert text2function (2)
-  ini=( ${ini[*]/%\} \)/\}} ) # remove extra parenthesis
+  IFS=$'\n' && ini=(${ini}) # convert to line-array
+  ini=(${ini[*]//;*/})      # remove comments with ;
+  ini=(${ini[*]/\    =/=})  # remove tabs before =
+  ini=(${ini[*]/=\   /=})   # remove tabs be =
+  ini=(${ini[*]/\ =\ /=})   # remove anything with a space around =
+  ini=(${ini[*]/#\\[/\}$'\n'config.section.}) # set section prefix
+  ini=(${ini[*]/%\\]/ \(})    # convert text2function (1)
+  ini=(${ini[*]/=/=\( })    # convert item to array
+  ini=(${ini[*]/%/ \)})     # close array parenthesis
+  ini=(${ini[*]/%\\ \)/ \\}) # the multiline trick
+  ini=(${ini[*]/%\( \)/\(\) \{}) # convert text2function (2)
+  ini=(${ini[*]/%\} \)/\}}) # remove extra parenthesis
   ini[0]="" # remove first element
   ini[${#ini[*]} + 1]='}'    # add the last brace
   eval "$(echo "${ini[*]}")" # eval the result
 }
 
-getProxyAuth() {
+proxy_auth() {
   if [ ! -z $proxy_user ]; then
     echo "--proxy-user=$proxy_user --proxy-password=$proxy_password "
   fi
 }
 
-# Shows the wget download status progress
-# @param wget output log file path
-# @param completed file to check
-downloadStatus() {
+download_status() {
   if [ -f $1 ]; then
     while : ; do
       sleep 1
@@ -106,16 +103,16 @@ downloadStatus() {
       local speed=$(echo `cat $1 | grep -oh '\([0-9.]\+[%].*[0-9.][s|m|h|d]\)' | tail -1`)
       echo -n "Downloading... $speed"
       echo -n R | tr 'R' '\r'
-      # evaluate exit code?
+
       if [ -f $2 ]; then
         sleep 1
         local error=$(echo `cat $2`)
         if [ $error != '0' ]; then
           echo 
           if [ $error == '6' ]; then
-            echo "Server authentication error, configure setup.ini properly. See $OUTPUTLOG"
+            echo "Server authentication error, configure setup.ini properly. See $out_log"
           else
-            echo "Download error, exit code '$error'. See $OUTPUTLOG"
+            echo "Download error, exit code '$error'. See $out_log"
           fi
           exit $?
         fi
@@ -138,19 +135,19 @@ if [ "`id -u`" -ne 0 ]; then
 fi
 
 if [ ! -z $1 ] && [ -f $1 ]; then
-  $CONFIGFILE="$1"
+  $config_file="$1"
 fi
 
 # disabling SELinux if enabled
-#if [ -f "/usr/sbin/getenforce" ] ; then
-#    selinux_status=`/usr/sbin/getenforce`
-#    /usr/sbin/setenforce 0 2> /dev/null
-#fi
+if [ -f "/usr/sbin/getenforce" ] ; then
+    selinux_status=`/usr/sbin/getenforce`
+    /usr/sbin/setenforce 0 2> /dev/null
+fi
 
 # read config file
-if [ -f $CONFIGFILE ]; then
-  configParser $CONFIGFILE
-  checkExitCode "Error while parsing config ini file: $CONFIGFILE"
+if [ -f $config_file ]; then
+  config_parser $config_file
+  check_exit "Error while parsing config ini file: $config_file"
 
   # load config variables
   if [ `exists config.section.auth` -eq 1 ]; then
@@ -168,15 +165,15 @@ if [ -f $CONFIGFILE ]; then
   fi
   
   if [ ! -z $fs_install ]; then
-    INSTALLDIR=$fs_install
+    install_dir=$fs_install
   fi
 fi
 
 # creates temporal directories
-makeDir $TEMPDIR
-makeDir $DOWNLOADSDIR
+make_dir $temporal
+make_dir $download_dir
 
-if [ -d $INSTALLDIR ] && [ -f $INSTALLDIR/VERSION ]; then
+if [ -d $install_dir ] && [ -f $install_dir/VERSION ]; then
   echo "FrontStack is already installed. Nothing to do"
   exit 0
 fi
@@ -186,8 +183,6 @@ cat <<EOF
  -------------------------------------
          Welcome to FrontStack
  -------------------------------------
-  
-     Setup and provisioning script
 
  VM requirements:
   * GNU/Linux 64 bits
@@ -198,24 +193,24 @@ cat <<EOF
 
 EOF
 
-wget $(getProxyAuth) http://yahoo.com -O $DOWNLOADSDIR/test.html > $OUTPUTLOG 2>&1
-checkExitCode "No Internet HTTP connectivity. Check if you are behind a proxy and your authentication credentials. See $OUTPUTLOG"
-rm -f $DOWNLOADSDIR/test.*
+wget $(proxy_auth) http://yahoo.com -O $download_dir/test.html > $out_log 2>&1
+check_exit "No Internet HTTP connectivity. Check if you are behind a proxy and your authentication credentials. See $out_log"
+rm -f $download_dir/test.*
 
 if [ $fs_format == '7z' ] || [ $fs_format == 'zip' ]; then
   if [ `exists 7z` -eq 0 ]; then
-    if [ ! -f $TEMPDIR/7zip/7z ]; then
+    if [ ! -f $temporal/7zip/7z ]; then
       echo -n "Downloding 7z... "
-      wget $(getProxyAuth) $SZIPURL -O $DOWNLOADSDIR/7z.tar.gz >> $OUTPUTLOG 2>&1
-      checkExitCode "Error while trying to download 7z. See $OUTPUTLOG"
+      wget $(proxy_auth) $szip_url -O $download_dir/7z.tar.gz >> $out_log 2>&1
+      check_exit "Error while trying to download 7z. See $out_log"
       echo "done!"
 
       echo -n "Extracting 7z... "
-      makeDir $TEMPDIR/7zip/
-      tar xvfz $DOWNLOADSDIR/7z.tar.gz -C $TEMPDIR/7zip/ >> $OUTPUTLOG 2>&1
-      checkExitCode "Error while trying to extract 7z.tar.gz. See $OUTPUTLOG"
+      make_dir $temporal/7zip/
+      tar xvfz $download_dir/7z.tar.gz -C $temporal/7zip/ >> $out_log 2>&1
+      check_exit "Error while trying to extract 7z.tar.gz. See $out_log"
       echo "done!"
-      COMPRESSBIN=$TEMPDIR/7zip/7z
+      COMPRESSBIN=$temporal/7zip/7z
     fi
   fi
 elif [ $fs_format == 'rpm' ]; then
@@ -232,19 +227,19 @@ echo "Downloading lastest version of the FrontStack dev environment"
 echo "Note this may take some minutes depending on your connection bandwidth... "
 echo 
 
-if [ -f $DOWNLOADSDIR/download ]; then
-  rm -f $DOWNLOADSDIR/download
+if [ -f $download_dir/download ]; then
+  rm -f $download_dir/download
 fi
 
 # download stack distribution
 if [ -z $fs_http_user ]; then
-    `wget $(getProxyAuth) -F $fs_download -O $DOWNLOADSDIR/frontstack-latest.$fs_format > $OUTPUTLOG 2>&1 && echo $? > $DOWNLOADSDIR/download || echo $? > $DOWNLOADSDIR/download` &
-    downloadStatus $OUTPUTLOG $DOWNLOADSDIR/download
+    `wget $(proxy_auth) -F $fs_download -O $download_dir/frontstack-latest.$fs_format > $out_log 2>&1 && echo $? > $download_dir/download || echo $? > $download_dir/download` &
+    download_status $out_log $download_dir/download
 else
-  `wget $(getProxyAuth) -F --user=$fs_http_user --password=$fs_http_password $fs_download -O $DOWNLOADSDIR/frontstack-latest.$fs_format > $OUTPUTLOG 2>&1  && echo $? > $DOWNLOADSDIR/download || echo $? > $DOWNLOADSDIR/download` &
-  downloadStatus $OUTPUTLOG $DOWNLOADSDIR/download
+  `wget $(proxy_auth) -F --user=$fs_http_user --password=$fs_http_password $fs_download -O $download_dir/frontstack-latest.$fs_format > $out_log 2>&1  && echo $? > $download_dir/download || echo $? > $download_dir/download` &
+  download_status $out_log $download_dir/download
 fi
-checkExitCode "Error while trying to download FrontStack. See $OUTPUTLOG"
+check_exit "Error while trying to download FrontStack. See $out_log"
 
 if [ $fs_format == 'rpm' ]; then
   if [ `exists rpm` -eq 0 ]; then
@@ -253,40 +248,40 @@ if [ $fs_format == 'rpm' ]; then
   fi
 
   echo -n "Installing RPM... "
-  rpm -ivh $DOWNLOADSDIR/frontstack-latest.$fs_format >> $OUTPUTLOG 2>&1
-  checkExitCode "Error while trying to install the RPM. See $OUTPUTLOG"
+  rpm -ivh $download_dir/frontstack-latest.$fs_format >> $out_log 2>&1
+  check_exit "Error while trying to install the RPM. See $out_log"
   echo 'done!'
 else
   echo -n "Extracting (this may take some minutes)... "
-  makeDir $INSTALLDIR
+  make_dir $install_dir
   if [ $fs_format == '7z' ]; then
-    $COMPRESSBIN e -o$INSTALLDIR -y $DOWNLOADSDIR/frontstack-latest.$fs_format >> $OUTPUTLOG 2>&1
+    $COMPRESSBIN e -o$install_dir -y $download_dir/frontstack-latest.$fs_format >> $out_log 2>&1
   else
-    $COMPRESSBIN xvfz $DOWNLOADSDIR/frontstack-latest.$fs_format -C $INSTALLDIR >> $OUTPUTLOG 2>&1
+    $COMPRESSBIN xvfz $download_dir/frontstack-latest.$fs_format -C $install_dir >> $out_log 2>&1
   fi
-  checkExitCode "Error while trying to extract FrontStack. See $OUTPUTLOG"
+  check_exit "Error while trying to extract FrontStack. See $out_log"
   echo 'done!'
 fi
 
 # set file permissions (by default Vagrant uses the root user to run the provisioning tasks/scripts)
 if [ ! -z $fs_user ]; then
   echo "Setting permissions for the '$fs_user' user..."
-  chown -R $fs_user:users $INSTALLDIR >> $OUTPUTLOG
-  checkExitCode "Error while trying to set files permissions. See $OUTPUTLOG"
+  chown -R $fs_user:users $install_dir >> $out_log
+  check_exit "Error while trying to set files permissions. See $out_log"
 
   # load FrontStack environment variables at session startup (.bash_profile, .profile, .bashrc)
   if [ $fs_bash_profile == '1' ]; then
     if [ -d "/home/$fs_user" ]; then
       if [ -f "/home/$fs_user/.bash_profile" ]; then
-        if [ $(exists `cat /home/$fs_user/.bash_profile | grep $INSTALLDIR`) -eq 0 ]; then
-          echo ". $INSTALLDIR/bash.sh" >> "/home/$fs_user/.bash_profile"
+        if [ $(exists `cat /home/$fs_user/.bash_profile | grep $install_dir`) -eq 0 ]; then
+          echo ". $install_dir/bash.sh" >> "/home/$fs_user/.bash_profile"
         fi
       else
         # creates a new bash session profile
         echo "#!/bin/bash" > "/home/$fs_user/.bash_profile"
-        echo ". $INSTALLDIR/bash.sh" >> "/home/$fs_user/.bash_profile"
+        echo ". $install_dir/bash.sh" >> "/home/$fs_user/.bash_profile"
         # set permissions
-        chown $fs_user:users "/home/$fs_user/.bash_profile" >> $OUTPUTLOG
+        chown $fs_user:users "/home/$fs_user/.bash_profile" >> $out_log
         chmod +x "/home/$fs_user/.bash_profile"
       fi
     fi
@@ -299,8 +294,8 @@ for pkg in "${install_packages[@]}"
 do
   if [ `exists "$pkg"` -eq 0 ]; then
     echo "Installing $pkg..."
-    installPackage $pkg >> $OUTPUTLOG 2>&1
-    checkExitCode "Cannot install '$pkg' package. See $OUTPUTLOG"
+    install_package $pkg >> $out_log 2>&1
+    check_exit "Cannot install '$pkg' package. See $out_log"
   fi
 done
 
@@ -309,9 +304,15 @@ if [ ! -n $install_script ] && [ -f $install_script ]; then
   . "$install_script"
 fi
 
+# re-enable SELinux
+if [ -f "/usr/sbin/getenforce" ] ; then
+    selinux_status=`/usr/sbin/getenforce`
+    /usr/sbin/setenforce 1 2> /dev/null
+fi
+
 cat <<EOF
 
-FrontStack installed in: "$INSTALLDIR"
+FrontStack installed in: "$install_dir"
 
 To have fun, simply run:
 $ vagrant ssh
